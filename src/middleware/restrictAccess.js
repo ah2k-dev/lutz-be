2c05275b4da57946f345a6f2e0b9b2d0d36efcc4
ahmedhassan2k <ah2k.dev@gmail.com>
2024-02-15 03:29:04 +0500

    cc

--- src/middleware/restrictAccess.js
+++ src/middleware/restrictAccess.js
@@ -1,11 +1,11 @@
 module.exports = (req, res, next) => {
-    const allowOrigins = [
-      "http://localhost:3000",
-      "http://localhost:3001",
-    ]; // add your own domains here
-    // uncomment before deployment
-    // console.log(req.headers["origin"]);
-    // if (!allowOrigins.includes(req.headers["origin"]) || !req.headers["origin"]) {
+    // const allowOrigins = [
+    //   "http://localhost:3000",
+    //   "http://localhost:3001",
+    // ]; // add your own domains here
+    // // uncomment before deployment
+    // // console.log(req.headers["origin"]);
+    // if (!allowOrigins.includes(req.headers["origin"])) {
     //   return res.status(401).json({
     //     message: "Not Authenticated",
     //   });
